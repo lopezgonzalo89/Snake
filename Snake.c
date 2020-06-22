d09ce19c3b4f76a06e5c30a773685e1cc4bf4ffd
Lopez Gonzalo <lopezgonzalo.sm89@gmail.com>
2020-06-22 19:12:14 -0300

    Movimientos

--- Snake.c
+++ Snake.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 #define V 21
 #define H 65
 #define N 100
@@ -9,12 +11,12 @@ typedef struct Snake
     int x, y;
     int ModX, ModY;
     char imagen;
-}snk;
+} snk;
 
-typedef struct 
+typedef struct
 {
     int x, y;
-}frt;
+} frt;
 
 snk snake[N];
 frt fruta;
@@ -22,22 +24,27 @@ frt fruta;
 // Prototipos de funciones
 void inicio(int *tam, char campo[V][H]);
 void Intro_Campo(char campo[V][H]);
-void Intro_Datos( char campo[V][H], int tam);
+void Intro_Datos(char campo[V][H], int tam);
 void draw(char campo[V][H]);
+void loop(char campo[H][V], int tam);
+void input(char campo[H][V], int *tam, int *muerto);
+void input(char campo[H][V], int *tam, int *muerto);
 
-int main(){
+int main()
+{
     int tam;
     char campo[V][H];
 
     inicio(&tam, campo);
 
-    draw(campo);
+    loop(campo, tam);
 
     system("pause");
     return 0;
 }
 
-void inicio(int *tam, char campo[V][H]){
+void inicio(int *tam, char campo[V][H])
+{
     int i;
 
     // snake[0] = Cabeza
@@ -50,14 +57,21 @@ void inicio(int *tam, char campo[V][H]){
     //Coordenadas de la fruta
     srand(time(NULL));
 
-    while( fruta.x == 0){
+    fruta.x = rand() % (H - 1);
+    fruta.y = rand() % (V - 1);
+
+    while (fruta.x == 0)
+    {
         fruta.x = rand() % (H - 1);
     };
-    while( fruta.y == 0){
+    while (fruta.y == 0)
+    {
         fruta.y = rand() % (V - 1);
     };
-    
-    for( i =0; i < *tam; i++){
+
+    // Movimiento
+    for (i = 0; i < *tam; i++)
+    {
         snake[i].ModX = 1;
         snake[i].ModY = 0;
     }
@@ -68,18 +82,24 @@ void inicio(int *tam, char campo[V][H]){
 }
 
 // Creacion del campo
-void Intro_Campo(char campo[V][H]){
+void Intro_Campo(char campo[V][H])
+{
     int i, j;
 
-    for(i = 0; i < V; i++){
-        for(j = 0; j < H; j++){
-            if ( i == 0 || i == V - 1){
+    for (i = 0; i < V; i++)
+    {
+        for (j = 0; j < H; j++)
+        {
+            if (i == 0 || i == V - 1)
+            {
                 campo[i][j] = '_';
             }
-            else if (j == 0 || j == H - 1){
-                campo [i][j] = '|';
+            else if (j == 0 || j == H - 1)
+            {
+                campo[i][j] = '|';
             }
-            else {
+            else
+            {
                 campo[i][j] = ' ';
             }
         }
@@ -87,14 +107,16 @@ void Intro_Campo(char campo[V][H]){
 }
 
 // Insertar datos en la matriz del campo
-void Intro_Datos( char campo[V][H], int tam){
+void Intro_Datos(char campo[V][H], int tam)
+{
     int i;
 
     // Cabeza de la snake
     snake[0].imagen = 'O';
 
     // Cuerpo de la snake
-    for(i = 1; i < tam; i++){
+    for (i = 1; i < tam; i++)
+    {
         snake[i].x = snake[i - 1].x - 1;
         snake[i].y = snake[i - 1].y;
 
@@ -103,21 +125,104 @@ void Intro_Datos( char campo[V][H], int tam){
     }
 
     // Dibuja la snake en el campo
-    for(i = 0; i < tam; i++){
-        campo[snake[i].y][snake[i].x] = snake[i].imagen;        
+    for (i = 0; i < tam; i++)
+    {
+        campo[snake[i].y][snake[i].x] = snake[i].imagen;
     }
 
-    // Dibuja la frurta en el campo
+    // Dibuja la fruta en el campo
     campo[fruta.y][fruta.x] = '%';
 }
 
-void draw(char campo[V][H]){
+void draw(char campo[V][H])
+{
     int i, j;
 
-    for( i = 0; i < V; i++){
-        for( j = 0; j < H; j++){
+    for (i = 0; i < V; i++)
+    {
+        for (j = 0; j < H; j++)
+        {
             printf("%c", campo[i][j]);
         }
         printf("\n");
+    }
+}
+
+void loop(char campo[H][V], int tam)
+{
+    int muerto;
+    muerto = 0;
+
+    do
+    {
+        draw(campo);
+        input(campo, &tam, &muerto);
+    } while (muerto == 0);
+}
+
+void input(char campo[H][V], int *tam, int *muerto)
+{
+    int i;
+    char key;
+
+    // Snake choca con un limite
+    if (snake[0].x == 0 || snake[0].y == 0 ||
+        snake[0].x == H - 1 || snake[0].y == V - 1)
+    {
+        *muerto = 1;
+    }
+    // Snake choca con su cuerpo
+    for (i = 1; i < *tam && *muerto == 0; i++)
+    {
+        if (snake[0].x == snake[i].x && snake[0].y == snake[i].y)
+        {
+            *muerto = 1;
+        }
+    }
+
+    // Obtener la frurta
+    if (snake[0].x = fruta.x && snake[0].y == fruta.y)
+    {
+        // Crece Snake
+        *tam += 1;
+        snake[*tam - 1].imagen = 'X';
+
+
+        // Nueva fruta
+        fruta.x = rand() % (H - 1);
+        fruta.y = rand() % (V - 1);
+
+        while (fruta.x == 0)
+        {
+            fruta.x = rand() % (H - 1);
+        };
+        while (fruta.y == 0)
+        {
+            fruta.y = rand() % (V - 1);
+        };
+    }
+
+    if(*muerto == 0){
+        if(kbhit() == 1){
+            // Obtener tecla presionada
+            key = getch();
+
+            if(key == '2'){
+                snake[0].ModX = 0;
+                snake[0].ModY = 1;
+            }
+            if(key == '8'){
+                snake[0].ModX = 0;
+                snake[0].ModY = -1;
+            }
+            if(key == '4'){
+                snake[0].ModX = -1;
+                snake[0].ModY = 0;
+            }
+            if(key == '6'){
+                snake[0].ModX = 1;
+                snake[0].ModY = 0;
+            }
+        }
     }
 }
